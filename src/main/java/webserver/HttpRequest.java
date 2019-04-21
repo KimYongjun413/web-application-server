d74e874b5829e262d6710363a3e40a604130befd
kimyongjun413 <kimyongjun413@gmail.com>
2019-04-21 18:36:22 +0900

    5장. 요청 데이터를 처리하는 로직을 별도의 클래스로 분리 해결

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,5 +1,8 @@
 package webserver;
 
+import util.HttpRequestUtils;
+import util.IOUtils;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,23 +20,77 @@ public class HttpRequest {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = bufferedReader.readLine();
 
-            //HTTP 메소드 분리
+            setMethod(line);
 
-            //URL 분리
+            setPath(line);
+            setGetParameter(line);
 
-            //헤더 분리
+            line = bufferedReader.readLine();
 
-            //본문 분리
+            setHeader(line, bufferedReader);
+
+            setPostParameter(line, bufferedReader);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void setMethod(String line) {
+        String method = line.split(" ")[0];
+        this.method = method;
+    }
+
+    private void setPath(String line) {
+        String[] pathSplitted = line.split(" ");
+        if(getMethod().equals("POST")) {
+            this.path = pathSplitted[1];
+            return;
+        }
+
+        int index = pathSplitted[1].indexOf("?");
+        if( index == -1) {
+            this.path = pathSplitted[1];
+        }else {
+            this.path = pathSplitted[1].substring(0,index);
+        }
+
+    }
+
+    private void setHeader(String line, BufferedReader br) {
+        while(line != null && !line.equals("")) {
+            String[] headerSplitted = line.split(":");
+            this.headerMap.put(headerSplitted[0].trim(), headerSplitted[1].trim());
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setGetParameter(String line) {
+        String[] pathSplitted = line.split(" ");
+        int index = pathSplitted[1].indexOf("?");
+        if( index > -1) {
+            parameterMap = HttpRequestUtils.parseQueryString((pathSplitted[1].substring(index+1)));
+        }
+    }
+
+    private void setPostParameter(String line, BufferedReader br) {
+        try {
+            if(!getMethod().equals("POST")) return;
+            String body = IOUtils.readData(br, Integer.parseInt(headerMap.get("Content-Length")));
+            this.parameterMap = HttpRequestUtils.parseQueryString(body);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public String getMethod() {
         return method;
     }
-
 
     public String getPath() {
         return path;
