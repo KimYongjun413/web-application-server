20636d51d98f1d95e07d02a87483c8247b8e9363
kimyongjun413 <kimyongjun413@gmail.com>
2019-04-22 23:24:49 +0900

    HttpResponse 작성 - 모르겠는건 일단 구현 및 설명을 보고 따라 침

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,19 +1,96 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpResponse {
+    private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
 
+    private Map<String, String> headerMap = new HashMap<>();
+    private DataOutputStream dos = null;
 
     public HttpResponse(OutputStream outputStream) {
+        dos = new DataOutputStream(outputStream);
     }
 
-    public void forward(String s) {
+    public void forward(String url) {
+        try {
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            if(url.endsWith(".css")) {
+                headerMap.put("Content-Type","text/css");
+            } else if(url.endsWith(".js")) {
+                headerMap.put("Content-Type","application/javascript");
+            } else {
+                headerMap.put("Content-Type", "text/html;charset=utf-8");
+            }
+            headerMap.put("Content-Length",body.length + "");
+            response200Header(body.length);
+            responseBody(body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
-    public void sendRedirect(String s) {
+    public void forwardBody(String body) {
+        byte[] contents = body.getBytes();
+        headerMap.put("Content-Type", "text/html;charset=utf-8");
+        headerMap.put("Content-Length",contents.length + "");
+        response200Header(contents.length);
+        responseBody(contents);
     }
 
-    public void addHeader(String s, String s1) {
+    public void sendRedirect(String redirectUrl) {
+        try{
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            processHeaders();
+            dos.writeBytes("Location: " + redirectUrl + " \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            processHeaders();
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void processHeaders() {
+        try {
+            Set<String> keys = headerMap.keySet();
+            for(String key : keys) {
+                dos.writeBytes(key + ": " + headerMap.get(key) + " \r\n");
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void addHeader(String key, String value) {
+        headerMap.put(key, value);
     }
 }
