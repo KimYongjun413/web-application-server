48b50b6bccdd061732ca78708b8c38f832048ffa
kimyongjun413 <kimyongjun413@gmail.com>
2019-04-23 23:33:34 +0900

    createUser, login, listUser 의 구현 코드를 Controller 인터페이스에 대한 구현 클래스로 이동

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.User;
+import db.DataBase;
+import http.HttpRequest;
+import http.HttpResponse;
+
+public class LoginController implements Controller {
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        User user = DataBase.findUserById(request.getParameter("userId"));
+        if (user != null) {
+            if (user.login(request.getParameter("password"))) {
+                response.addHeader("Set-Cookie", "logined=true");
+                response.sendRedirect("/index.html");
+            } else {
+                response.sendRedirect("/user/login_failed.html");
+            }
+        } else {
+            response.sendRedirect("/user/login_failed.html");
+        }
+    }
+}
