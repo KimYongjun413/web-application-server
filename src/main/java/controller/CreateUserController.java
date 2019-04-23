3043033ba58e316b04d67ed69f7808f110f705e4
kimyongjun413 <kimyongjun413@gmail.com>
2019-04-23 23:50:34 +0900

    웹 서버 리팩토링 실습 완료

--- src/main/java/controller/CreateUserController.java
+++ src/main/java/controller/CreateUserController.java
@@ -9,11 +9,11 @@ import org.slf4j.LoggerFactory;
 
 import db.DataBase;
 
-public class CreateUserController implements Controller {
+public class CreateUserController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
 
     @Override
-    public void service(HttpRequest request, HttpResponse response) {
+    public void doPost(HttpRequest request, HttpResponse response) {
         User user = new User(
                 request.getParameter("userId"),
                 request.getParameter("password"),
